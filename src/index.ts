1a3e9f904247522d8a50b0105397e6d602045fdd
mnmallea <mnmallea@gmail.com>
2019-11-10 21:43:30 -0300

    time measurement

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,22 @@ const writeFile = util.promisify(fs.writeFile);
 const args = process.argv.slice(2);
 const filename = args[0];
 
+function humanFileSize(bytes: number, si: boolean = false) {
+  const thresh = si ? 1000 : 1024;
+  if (Math.abs(bytes) < thresh) {
+    return bytes + " B";
+  }
+  const units = si
+    ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  let u = -1;
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+  return bytes.toFixed(1) + " " + units[u];
+}
+
 async function processFile(filename: string) {
   const rl = readline.createInterface({
     input: process.stdin,
@@ -22,6 +38,16 @@ async function processFile(filename: string) {
     });
   };
 
+  console.log(`Encriptando ${filename}`);
+
+  const key = await question("Ingrese la clave ultrasecreta: ");
+  console.log(`Clave: ${key}`);
+
+  const iv = await question("Ingrese el IV: ");
+  console.log(`IV: ${iv}`);
+
+  const start = process.hrtime();
+
   let file: Buffer | undefined;
 
   try {
@@ -32,19 +58,14 @@ async function processFile(filename: string) {
     process.exit(1);
   }
 
-  console.log(`Encriptando ${filename}`);
-
-  const key = await question("Ingrese la clave ultrasecreta: ");
-  console.log(`Clave: ${key}`);
-
-  const iv = await question("Ingrese el IV: ");
-  console.log(`IV: ${iv}`);
-
   const cipherData = cipher.cipher(file as Buffer, key, iv);
   const outputFile = `${filename}.ciph`;
   await writeFile(outputFile, cipherData);
 
-  console.log(`Output sent to ${outputFile}`);
+  const end = process.hrtime(start);
+  console.log(`\nArchivo cifrado en ${outputFile}`);
+  console.log(`Cifrados ${humanFileSize(cipherData.length)} en %ds %dms`, end[0], end[1] / 1000000);
+  console.log(`Velocidad: ${(cipherData.length * 8 / (end[0] + end[1] / 1e9)) / 1e6} Mbps`);
 
   process.exit(0);
 }
